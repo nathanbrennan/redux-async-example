b81d0cdd1e28609d3e2c8ffc9c863621df45076d
Nathan Brennan <nathan_brennan@flightcentre.com>
2016-04-05 07:13:30 +1000

    Move reducers index

--- src/js/reducers/index.js
+++ src/js/reducers/index.js
@@ -7,17 +7,55 @@ import {
 
 const initialState = {
   selectedSubreddit: '',
-  entities: {
-    users: {},
-    posts: {}
-  },
+  usersById: {},
+  postsById: {}
   postsBySubreddit: {}
+}
+
+const selectedSubreddit = (state = 'games', action) {
+  switch (action.type) {
+    case SELECT_SUBREDDIT:
+      state = action.subreddit
+      break;
+    default:
+      state
+  }
+}
+
+const postsById = (state = {}, action) => {
+  switch (action.type) {
+    case RECEIVE_POSTS:
+      return Object.assign({}, state, action.posts)
+      break;
+    default:
+      state
+  }
+}
+
+const postsBySubreddit = (state = {}, action) => {
+  switch (action.type) {
+    case RECEIVE_POSTS:
+      // YOU ARE HERE
+      // YOU ARE HERE
+      // YOU ARE HERE
+      // YOU ARE HERE
+      break
+    case RECEIVE_POSTS:
+      //...
+      break
+    case INVALIDATE_SUBREDDIT:
+      //...
+      break
+    default:
+      state
+  }
 }
 
 const rootReducer = (state = initialState, action) => {
   return {
     selectedSubreddit: selectedSubreddit(state.selectedSubreddit, action),
-    entities: entities(state.entities, action),
+    usersById: usersById(state.usersById, action),
+    postsById: postsById(state.postsById, action),
     postsBySubreddit: postsBySubreddit(state.postsBySubreddit, action)
   }
 }
@@ -27,24 +65,22 @@ export default rootReducer
 /* EXAMPLE STATE...
 {
   selectedSubreddit: 'frontend',
-  entities: {
-    users: {
-      2: {
-        id: 2,
-        name: 'Andrew'
-      }
+  usersById: {
+    2: {
+      id: 2,
+      name: 'Andrew'
+    }
+  },
+  postsById: {
+    42: {
+      id: 42,
+      title: 'Confusion about Flux and Relay',
+      author: 2
     },
-    posts: {
-      42: {
-        id: 42,
-        title: 'Confusion about Flux and Relay',
-        author: 2
-      },
-      100: {
-        id: 100,
-        title: 'Creating a Simple Application Using React JS and Flux Architecture',
-        author: 2
-      }
+    100: {
+      id: 100,
+      title: 'Creating a Simple Application Using React JS and Flux Architecture',
+      author: 2
     }
   },
   postsBySubreddit: {
